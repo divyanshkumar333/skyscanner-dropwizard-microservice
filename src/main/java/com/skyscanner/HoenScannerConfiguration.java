01ca7e7bf4530af2762fb8eb71c7d1fba9b77a70
divyanshkumar333 <divyanshkumar027@gmail.com>
2026-09-02 00:12:25 +0530

    Incorporate model answer data and pom validation dependencies

---
+++ src/main/java/com/skyscanner/HoenScannerConfiguration.java
@@ -0,0 +1,8 @@
+package com.skyscanner;
+
+import io.dropwizard.core.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class HoenScannerConfiguration extends Configuration {
+    // Add configuration properties here if needed
+}
